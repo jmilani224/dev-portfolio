dd9098a8456a809a689d280033803bfd30617f39
jmilani224 <jmilani224@gmail.com>
2020-09-17 10:33:35 -0400

    added build

---
+++ build/precache-manifest.cee93fe42117e977e6f95e676238c331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffad5160ed062658ea99e0cd0e9d529b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a16e143230792811e40a",
+    "url": "/static/css/main.53efcd60.chunk.css"
+  },
+  {
+    "revision": "02d24e155b3ae23bd731",
+    "url": "/static/js/2.5a47c57b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a47c57b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4024a7fd7822fd4f7ffe",
+    "url": "/static/js/3.ec90c3eb.chunk.js"
+  },
+  {
+    "revision": "a16e143230792811e40a",
+    "url": "/static/js/main.01b26541.chunk.js"
+  },
+  {
+    "revision": "7765e5b014608a10711d",
+    "url": "/static/js/runtime-main.784f0fcc.js"
+  }
+]);
